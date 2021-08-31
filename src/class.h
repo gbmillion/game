ecf992781a0eec90060f3ef1937e2bfa22f3ead2
gbmillion <gbmillion@bucs.fsw.edu>
2021-08-31 17:43:52 -0400

    updating predefined classes

--- src/class.h
+++ src/class.h
@@ -8,10 +8,11 @@
 #ifndef CLASS_H_
 #define CLASS_H_
 // maybe add predefined classes at some point
+
 struct item{
 	char type[255];
 	char trait[255];
-	int amount;
+	int amount;//item id number, not amount
 };/*
 Format goes as is:
 type[string of name of type of item],trait[the class trait effected by the item],amount[item id number],
@@ -30,7 +31,44 @@ struct class{
 	int hp; //Implemented
 	int mana; //Implemented
 	struct item inventory[100];
+	char clas[25];
 };
-
-
+void load_clas(struct class *toon){
+	char class_name[25] = "mage";
+	if ( strcmp(  &class_name,  &toon->clas ) == 0 ){
+		toon->Dexterity=10;
+		toon->Stamina=10;
+		toon->Wisdom=10;
+		toon->Strength=10;
+		toon->Intelligence=18;
+		toon->Agility=16;
+	}
+	class_name[25] = "fighter";
+	if ( strcmp(  &class_name,  &toon->clas ) == 0 ){
+		toon->Dexterity=10;
+		toon->Stamina=16;
+		toon->Wisdom=10;
+		toon->Strength=18;
+		toon->Intelligence=10;
+		toon->Agility=10;
+	}
+	class_name[25] = "healer";
+	if ( strcmp(  &class_name,  &toon->clas ) == 0 ){
+		toon->Dexterity=10;
+		toon->Stamina=16;
+		toon->Wisdom=18;
+		toon->Strength=10;
+		toon->Intelligence=10;
+		toon->Agility=10;
+	}
+	class_name[25] = "rouge";
+	if ( strcmp(  &class_name,  &toon->clas ) == 0 ){
+		toon->Dexterity=18;
+		toon->Stamina=10;
+		toon->Wisdom=10;
+		toon->Strength=10;
+		toon->Intelligence=10;
+		toon->Agility=16;
+	}
+}
 #endif /* CLASS_H_ */
